8d6a8522fe30c000a350da21ade0c1ac6bbf696c
Will <woswald@aspireautism.com>
2022-04-18 20:58:41 -0400

    Added JS File

--- wwwroot/script.js
+++ wwwroot/script.js
@@ -1,1 +1,20 @@
 ﻿
+//Begin script for index.html
+$(document).ready(function () {
+    var accordArray = document.getElementsByClassName("accordion");
+
+    console.log(accordArray);
+    for (var i = 0; i < accordArray.length; i++) {
+        accordArray[i].addEventListener("click", function () {
+            //Make Active when Clicked//vice versa
+            this.classList.toggle("active");
+
+            var details = this.nextElementSibling;
+            if (details.style.display === "block") {
+                details.style.display = "none";
+            } else {
+                details.style.display = "block";
+            }
+        });
+    }
+});
